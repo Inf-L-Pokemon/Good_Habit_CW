04b33e2ff394dd047373346f6c4b1b6f96ee2852
Sergey Korchmin <sergey.korchmin@yandex.ru>
2024-10-25 00:45:15 +0300

    Написаны тесты приложения.

--- habits/tests.py
+++ habits/tests.py
@@ -1,3 +1,71 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from habits.models import Habit
+from users.models import User
+
+
+class HabitTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create(email="test@example.com")
+        self.habit = Habit.objects.create(
+            place="дом",
+            time="08:00:00",
+            action="делать зарядку",
+            is_pleasant_habit=False,
+            frequency=1,
+            award="съесть яблоко",
+            execution_time=120,
+            is_public=True,
+            owner=self.user,
+        )
+        self.client.force_authenticate(user=self.user)
+
+    def test_habit_retrieve(self):
+        url = reverse("habits:habit-detail", args=(self.habit.pk,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.json()
+        self.assertEqual(data.get("action"), self.habit.action)
+
+    def test_habit_create(self):
+        url = reverse('habits:habit-create')
+        data = {
+            "place": "дом",
+            "time": "20:40:00",
+            "action": "протереть пыль",
+            "is_pleasant_habit": False,
+            "frequency": 1,
+            "award": "посмотреть фильм",
+            "execution_time": 120,
+            "is_public": True
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Habit.objects.all().count(), 2)
+
+    def test_habit_list(self):
+        url = reverse("habits:habit-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_habit_update(self):
+        url = reverse("habits:habit-update", args=(self.habit.pk,))
+        data = {
+            "award": "посмотреть фильм",
+        }
+        response = self.client.patch(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(data.get("award"), "посмотреть фильм")
+
+    def test_public_habit_list(self):
+        url = reverse("habits:habit-public-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_habit_delete(self):
+        url = reverse("habits:habit-delete", args=(self.habit.pk,))
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Habit.objects.all().count(), 0)
--- habits/views.py
+++ habits/views.py
@@ -10,7 +10,6 @@ from users.permissions import IsOwner
 class HabitCreateAPIView(generics.CreateAPIView):
     serializer_class = HabitSerializer
     queryset = Habit.objects.all().order_by('-id')
-    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -20,7 +19,7 @@ class HabitListAPIView(generics.ListAPIView):
     serializer_class = HabitSerializer
     queryset = Habit.objects.all().order_by('-id')
     pagination_class = HabitPagination
-    permission_classes = [IsAuthenticated, IsOwner]
+    permission_classes = [IsOwner]
 
 
 class HabitPublicListAPIView(generics.ListAPIView):
@@ -32,15 +31,15 @@ class HabitPublicListAPIView(generics.ListAPIView):
 class HabitDetailAPIView(generics.RetrieveAPIView):
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
-    permission_classes = [IsAuthenticated, IsOwner]
+    permission_classes = [IsOwner]
 
 
 class HabitUpdateAPIView(generics.UpdateAPIView):
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
-    permission_classes = [IsAuthenticated, IsOwner]
+    permission_classes = [IsOwner]
 
 
 class HabitDeleteAPIView(generics.DestroyAPIView):
     queryset = Habit.objects.all()
-    permission_classes = [IsAuthenticated, IsOwner]
+    permission_classes = [IsOwner]
